e34510db80e8f7f0b9a29efd85d5d238d72e8457
Yura Petrenko <107058776+YuraPetrenko84@users.noreply.github.com>
2022-10-19 19:21:26 +0300

    Update 5.js

--- js/5.js
+++ js/5.js
@@ -460,3 +460,6 @@
 // console.log(mango.blacklistedEmails); // ["poly@mail.com"]
 // console.log(mango.isBlacklisted("mango@mail.com")); // false
 // console.log(mango.isBlacklisted("poly@mail.com")); // true
+
+
+
